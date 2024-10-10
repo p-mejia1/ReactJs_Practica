d79055542de6421d213ed814acbc3a1ad6aa5931
p-mejia1 <pedro.mejia@prontobpo.com>
2024-10-10 11:03:30 -0600

    Update App.jsx
    CAMBIO LEVEL1

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     <>
     <Usuario nombreUsuario="David" 
     apellidoUsuario="Mejia" 
-    level={0}/>
+    level={1}/>
   
     <Row style={{backgroundColor: geekblue[5]}}>
       <Col span={24}>col-24</Col>
